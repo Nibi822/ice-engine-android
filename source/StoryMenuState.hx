30ca3dd700a3be6f270d6ed956731a59219f37b4
Nibi822 <107842914+Nibi822@users.noreply.github.com>
2022-06-22 19:43:58 +0200

    Add files via upload

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -221,15 +221,7 @@ class StoryMenuState extends MusicBeatState
 		if (!movedBack && !selectedWeek)
 		{
 			var upP = controls.UI_UP_P;
-			var downP = controls.UI_DOWN_P;
-			if (FlxG.mouse.wheel != 0) 
-			{
-				FlxG.sound.play(Paths.sound('scrollMenu'));
-				changeWeek(-FlxG.mouse.wheel);
-			}
-			
-			if (upP)
-			{
+			var downP = controls.UI_DOWN_P;			{
 				changeWeek(-shiftMult);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 				holdTime = 0;
@@ -287,8 +279,6 @@ class StoryMenuState extends MusicBeatState
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
 				//FlxG.sound.play(Paths.sound('scrollMenu'));
-			}
-			else if (controls.ACCEPT || FlxG.mouse.justPressed)
 			{
 				selectWeek();
 			}
