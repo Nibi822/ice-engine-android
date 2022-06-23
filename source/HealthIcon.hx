69d593d1b02739ba46f83d42cca86f698f31c246
Nibi822 <107842914+Nibi822@users.noreply.github.com>
2022-06-23 13:56:34 +0200

    Add files via upload

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -34,21 +34,27 @@ class HealthIcon extends FlxSprite
 		else changeIcon('bf');
 	}
 
+	var iconVerPath:String;
 	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
+			iconVerPath = 'vanilla/';
+			if(ClientPrefs.iconVer) {
+				iconVerPath = 'mic\'d up/';
+			}
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/' + iconVerPath + 'icon-' + char; //Older versions of psych engine's support
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/' + iconVerPath + 'icon-face'; //Prevents crash from missing icon
 			var file:Dynamic = Paths.image(name);
 
 			loadGraphic(file); //Load stupidly first for getting the file size
-			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (width - 150) / 2;
+			loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr
+			iconOffsets[0] = (width - 150) / 3;
+			iconOffsets[1] = (width - 150) / 3;
+			iconOffsets[2] = (width - 150) / 3;
 			updateHitbox();
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 
